63636958fe56d167934dad194de202eb1ff7d6d6
roflox <wow.brog@gmail.com>
2020-01-08 18:43:32 +0100

    spousteci skripty

--- scripts/Node5.sh
+++ scripts/Node5.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar ../target/NodeImpl-jar-with-dependencies.jar -n Node5 -p 50002 -r 1101 -h 192.168.56.1 -A 192.168.56.104 -t Node3 -P 1099
+java -jar ../target/NodeImpl-jar-with-dependencies.jar -n Node5 -p 50002 -r 1101 -h 192.168.56.1 -A 192.168.56.103 -t Node2 -P 1099
